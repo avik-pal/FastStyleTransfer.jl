567eea282d5a3257640dd1c043475244ae3ce06a
Avik Pal <avikpal@iitk.ac.in>
2018-05-10 22:56:09 +0530

    Added VGGNet Architecture from Metalhead

---
+++ src/vgg.jl
@@ -0,0 +1,25 @@
+# The license for this code is available at https://github.com/avik-pal/FastStyleTransfer.jl/blob/master/LICENSE.md
+
+mutable struct vgg19
+    slice1
+    slice2
+    slice3
+    slice4
+end
+
+function vgg19()
+    vgg = VGG19().layers
+    slice1 = Chain(vgg[1:5]...)
+    slice2 = Chain(vgg[6:10]...)
+    slice3 = Chain(vgg[11:15]...)
+    slice4 = Chain(vgg[16:20]...)
+    vgg19(slice1, slice2, slice3, slice4)
+end
+
+function (layer::vgg19)(x)
+    res1 = layer.slice1(x)
+    res2 = layer.slice2(res1)
+    res3 = layer.slice3(res2)
+    res4 = layer.slice4(res3)
+    (res1, res2, res3, res4)
+end
