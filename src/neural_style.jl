89571e3e53015b7411b7c93cf004980defe797ad
Avik Pal <avikpal@iitk.ac.in>
2018-05-31 17:23:07 +0530

    Fix stylize function

--- src/neural_style.jl
+++ src/neural_style.jl
@@ -58,17 +58,19 @@ end
 
 function stylize(image_path, model_path = "../models/trained_network_1.bson"; save_path = None, display::Bool = true)
     info("Starting to Load Model")
-    @load model_path style_model
+    @load model_path transformer
+    transformer = transformer |> gpu
     info("Model has been Loaded Successfully")
     img = load_image(image_path)
+    img = reshape(img, size(img)..., 1) |> gpu
     info("Image Loaded Successfully")
     a = time()
-    stylized_img = style_model(img)
+    stylized_img = transformer(img)
     info("Image has been Stylized in $(time()-a) seconds")
     if(save_path == None)
         path = rsplit(image_path, ".", limit = 2)
         save_path = "$(path[1])_stylized.$(path[2])"
     end
-    save_image(save_path, stylized_img, display)
+    save_image(save_path, stylized_img.data |> cpu, display)
     info("The image has been saved successfully")
 end
